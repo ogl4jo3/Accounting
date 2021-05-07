065444a06b0f0bbdf73c25bd1725cfc91d1d7262
Willy <willy_zhan@mayohr.com>
2021-05-07 15:39:00 +0800

    update gradle and library version

--- app/src/main/kotlin/com/ogl4jo3/accounting/data/source/AppDatabase.kt
+++ app/src/main/kotlin/com/ogl4jo3/accounting/data/source/AppDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.RoomDatabase
 import com.ogl4jo3.accounting.data.Account
 import com.ogl4jo3.accounting.data.Category
 
-@Database(entities = [Category::class, Account::class], version = 1)
+@Database(entities = [Category::class, Account::class], version = 1, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun categoryDao(): CategoryDao
     abstract fun accountDao(): AccountDao
